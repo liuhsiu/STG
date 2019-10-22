1133654ebdde2e3e591ffe81f8d29f088f6a7c18
Caroline Lee <clee1@doterra.com>
2019-10-22 15:18:17 -0600

    Clean up challenge#4

--- challenge_4/challenge4.py
+++ challenge_4/challenge4.py
@@ -6,40 +6,8 @@ from page_object_functions.convert_num_to_string import ConvertNumToString
 
 
 class Challenge4(unittest.TestCase):
-
-    # def setUp(self):
-    #     self.driver = webdriver.Chrome()
-    #
-    # def tearDown(self):
-    #     self.driver.close()
-    #     print('in tear down method')
-
-    # def test_for_loop(self):
-    #     self.driver.get("https://www.copart.com")
-    #     popsearches = self.driver.find_elements(By.XPATH, "//*[@ng-if=\"popularSearches\"]/../div[3]//a")
-    #     #popsearches = self.driver.find_element(By.XPATH, "//*[@id='tabTrending']/div[1]")
-    #     print(len(popsearches))
-    #     for x in popsearches:
-    #         print(x.text + "-" + x.get_attribute("href"))
-    #
-    # def test_while_loop(self):
-    #     popcategories = self.driver.find_elements(By.XPATH, "//*[@ng-if=\"popularSearches\"]/../div[3]//a")
-    #     i = 0
-    #     while i < len(popcategories):
-    #         print(i)
-    #         i += 1
-    #         print(popcategories.index[i].text + "-" + popcategories[i].get_attribute("href"))
-
-
-
     # Program to display the Fibonacci sequence up to n-th term where n is provided by the user
     def test_recur_fibo(self):
-
-        # Change this value for a different result
-        #nterms = 10
-        #Fibonacci.recur_fibo(nterms)
-
-        # uncomment to take input from the user
         nterms = int(input("How many terms? "))
 
         # check if the number of terms is valid
