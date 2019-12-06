401d54d1272119780df3c22f0c93152b0d2b56d2
Caroline Lee <clee1@doterra.com>
2019-12-05 17:53:51 -0700

    Fixed challenge #4, will do million

--- page_object_functions/convert_num_to_string.py
+++ page_object_functions/convert_num_to_string.py
@@ -3,7 +3,7 @@ class ConvertNumToString:
 
     @staticmethod
     def convert_num_to_string(number):
-        singledigits = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+        singledigits = ["", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
         tens = [" ", " ", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"]
         teens = ["ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"]
 
@@ -11,22 +11,27 @@ class ConvertNumToString:
         if number == 0:
                 temp_str = "zero"
 
-        first_digit = number // 1000
-        second_digit = (number % 1000) // 100
-        third_digit = (number % 100) // 10
-        fourth_digit = (number % 10)
+        first_digit = number // 100000
+        second_digit = (number % 100000) // 10000
+        third_digit = (number % 10000) // 1000
+        fourth_digit = (number % 1000) // 100
+        fifth_digit = (number % 100)  // 10
+        sixth_digit = (number % 10)
 
         if first_digit  > 0:
-            temp_str = temp_str + singledigits[first_digit] + ' thousand '
-        if second_digit > 0:
-            temp_str = temp_str + singledigits[second_digit] + ' hundred and '
-        if third_digit > 1:
-            temp_str = temp_str + tens[third_digit] + " "
-        if third_digit == 1:
-            temp_str = temp_str + teens[fourth_digit] + " "
+            temp_str = temp_str + singledigits[first_digit] + ' hundred '
+        if second_digit >= 0:
+            temp_str = temp_str + tens[second_digit] + ' '
+        if third_digit >= 0:
+            temp_str = temp_str + singledigits[third_digit] + 'thousand '
+        if fourth_digit > 1:
+            temp_str = temp_str + singledigits[fourth_digit] + ' hundred '
+        if fifth_digit > 1:
+            temp_str = temp_str + tens[fifth_digit] + ' '
+        if sixth_digit > 0:
+            temp_str = temp_str + singledigits[sixth_digit]
+
         else:
-            if fourth_digit:
-                temp_str = temp_str + singledigits[fourth_digit] + " "
             if temp_str[-1] == " ":  # If the last number is a space
                 temp_str = temp_str[0:-1]  # Slice it
         return temp_str
