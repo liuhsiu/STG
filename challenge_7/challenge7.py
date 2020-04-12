70cc210749a1bf51a71e7e47385746b6173dc592
Caroline Lee <clee1@doterra.com>
2020-04-11 22:46:53 -0600

    Add Categories in Assignment#7

--- challenge_7/challenge7.py
+++ challenge_7/challenge7.py
@@ -28,17 +28,33 @@ class Challenge7(unittest.TestCase):
         self.driver.get("https://www.copart.com")
         self.driver.maximize_window()
 
-        make_row = WebDriverWait(self.driver, 60).until(
+        # Makes / Models
+        make_models = WebDriverWait(self.driver, 60).until(
             EC.presence_of_element_located((By.XPATH, "//*[@id=\"tabTrending\"]//div[2]")))
 
         # Print out each car name
-        make = make_row.text.split()
-        make_url = make_row.get_attribute("innerHTML")
+        make = make_models.text.split()
+        make_url = make_models.get_attribute("innerHTML")
 
-        # This will print all the html
+        # This will print all the make html
         for x in make:
             for y in x:
                 print(make_url)
+            print("\n")
+
+        # ------------------------------------------------------------------
+        # Categories
+        categories = WebDriverWait(self.driver, 60).until(
+            EC.presence_of_element_located((By.XPATH, "//*[@id=\"tabTrending\"]/div[3]")))
+
+        # Print out each car name
+        category = categories.text.split("\n")
+        category_url = categories.get_attribute("innerHTML")
+
+        # This will print all the make html
+        for x in category:
+            for y in x:
+                print(category_url)
             print("\n")
 
         # Scraper Class
@@ -52,12 +68,15 @@ class Challenge7(unittest.TestCase):
             def handle_data(self, data):
                 print("Data:", data)
 
-        parser = MyHTMLParser()
-
         #  with open(r'C:\Users\...site_1.html', "r") as f:
         #      page = f.read()
         # tree = make_url.fromstring()
+
+        parser = MyHTMLParser()
         parser.feed(make_url)
+
+        parser2 = MyHTMLParser()
+        parser2.feed(category_url)
 
 
 if __name__ == '__main__':
